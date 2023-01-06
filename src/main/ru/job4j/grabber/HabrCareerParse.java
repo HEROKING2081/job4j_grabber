dbe05402909658e187efee64ded98b5b0736e8b2
herok <heroking2081@gmail.com>
2023-01-06 16:04:02 +0500

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1 [#285210 #163690]

--- src/main/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/ru/job4j/grabber/HabrCareerParse.java
@@ -5,9 +5,12 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
+import ru.job4j.grabber.utils.DateTimeParser;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.time.LocalDateTime;
 
 public class HabrCareerParse {
 
@@ -15,20 +18,28 @@ public class HabrCareerParse {
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
+    private static final DateTimeParser DATE_TIME_PARSER = new HabrCareerDateTimeParser();
+
+    private static final int PAGES = 5;
+
     public static void main(String[] args) throws IOException {
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            System.out.printf("%s %s%n", vacancyName, link);
-            Element dateElement = row.select(".vacancy-card__date").first();
-            Element dateTimeElement = Objects.requireNonNull(dateElement).child(0);
-            String dataTime = dateTimeElement.attr("datetime");
-            System.out.println("Дата вакансии: " + dataTime);
-        });
+        for (int i = 1; i <= PAGES; i++) {
+            Connection connection = Jsoup.connect(String.format("%s%s", PAGE_LINK, "?page=" + i));
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                System.out.printf("%s %s%n", vacancyName, link);
+                Element dateElement = row.select(".vacancy-card__date").first();
+                Element dateTimeElement = Objects.requireNonNull(dateElement).child(0);
+                String dataTime = dateTimeElement.attr("datetime");
+                System.out.println("Дата вакансии: " + dataTime);
+                LocalDateTime localDateTime = DATE_TIME_PARSER.parse(dataTime);
+                System.out.println("Дата вакансии в формате для LocalDataTime: " + localDateTime);
+            });
+        }
     }
 }
